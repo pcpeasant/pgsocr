ca469892f85022da0f5bf66dce7cd591964aa749
pcpeasant <99271092+pcpeasant@users.noreply.github.com.>
2024-07-11 22:10:00 +0530

    Restructure project

---
+++ src/pgsocr/transformer_ocr_engines.py
@@ -0,0 +1,51 @@
+from PIL import Image
+from transformers import AutoProcessor, AutoModelForCausalLM
+import os
+import torch
+
+# workaround for unnecessary flash_attn requirement
+from unittest.mock import patch
+from transformers.dynamic_module_utils import get_imports
+
+
+def fixed_get_imports(filename: str | os.PathLike) -> list[str]:
+    if not str(filename).endswith("modeling_florence2.py"):
+        return get_imports(filename)
+    imports = get_imports(filename)
+    imports.remove("flash_attn")
+    return imports
+
+
+class Florence2OCREngine:
+    def __init__(self):
+        self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
+        model_id = "microsoft/Florence-2-large-ft"
+        with patch(
+            "transformers.dynamic_module_utils.get_imports", fixed_get_imports
+        ):  # workaround for unnecessary flash_attn requirement
+            self.model = AutoModelForCausalLM.from_pretrained(
+                model_id, attn_implementation="sdpa", trust_remote_code=True
+            ).to(self.device)
+        self.processor = AutoProcessor.from_pretrained(model_id, trust_remote_code=True)
+
+    def get_ocr_text(self, im: Image.Image):
+        task_prompt = "<OCR_WITH_REGION>"
+        inputs = self.processor(text=task_prompt, images=im, return_tensors="pt")
+
+        generated_ids = self.model.generate(
+            input_ids=inputs["input_ids"].to(self.device),
+            pixel_values=inputs["pixel_values"].to(self.device),
+            max_new_tokens=1024,
+            num_beams=3,
+            do_sample=False,
+        )
+        generated_text = self.processor.batch_decode(
+            generated_ids, skip_special_tokens=False
+        )[0]
+
+        parsed_answer = self.processor.post_process_generation(
+            generated_text, task=task_prompt, image_size=(im.width, im.height)
+        )
+        return "\n".join(
+            s.replace("</s>", "").strip() for s in parsed_answer[task_prompt]["labels"]
+        )
